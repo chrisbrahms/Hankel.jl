d040034c6c58ad48df28613ec60c57336dc40f18
Seth Axen <seth.axen@gmail.com>
2020-04-26 18:16:35 -0700

    Remove rare unicode characters

--- src/qdht.jl
+++ src/qdht.jl
@@ -19,9 +19,9 @@ but with some alterations:
 
 The transform matrix T is not the same as C/T defined in [1, 2] but is more like the form
 used in equation 14 of [3].
-Instead of dividing by j₁(αₚₙ)j₁(αₚₘ) we divide by j₁(αₚₙ)^2. This cancels out
-the factor between f and F so we do not have to mutltiply (divide) by j₁(αₚₙ) (j₁(αₚₘ)) before
-and after applying the transform matrix.
+Instead of dividing by ``J_1(α_{pn}) J_1(α_{pm})`` we divide by ``J_1(α_{pn})^2``.
+This cancels out the factor between ``f`` and ``F`` so we do not have to mutltiply (divide)
+by ``J_1(α_{pn})`` (``J_1(α_{pm})``) before and after applying the transform matrix.
 
 Follows [`AbstractFFT`](https://github.com/JuliaMath/AbstractFFTs.jl) approach of applying
 fwd and inv transform with `mul` and `ldiv`.
