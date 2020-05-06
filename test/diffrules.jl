24e9a8eb708e6d651001b7b3c853d20421be5a11
Seth Axen <seth.axen@gmail.com>
2020-05-05 22:41:05 -0700

    Test mutating and streamline tests

--- test/diffrules.jl
+++ test/diffrules.jl
@@ -11,13 +11,15 @@ function pullback_test(
 )
     # Check correctness of evaluation.
     xs, x̄s = collect(zip(xx̄s...))
-    y, pullback = Zygote.pullback((xs...) -> f(xs...; fkwargs...), xs...)
-    @test isapprox(y, f(xs...; fkwargs...); rtol = rtol, atol = atol, kwargs...)
+    xscopy = deepcopy(xs)
+    y = f(deepcopy(xs)...; fkwargs...)
+    y_ad, pullback = Zygote.pullback((xs...) -> f(xs...; fkwargs...), xs...)
+    @test isapprox(y_ad, y; rtol = rtol, atol = atol, kwargs...)
 
     x̄s_ad = pullback(ȳ)
 
     # Correctness testing via finite differencing.
-    return for (i, x̄_ad) in enumerate(x̄s_ad)
+    for (i, x̄_ad) in enumerate(x̄s_ad)
         if x̄s[i] === nothing
             @test x̄_ad === nothing
         else
@@ -29,107 +31,92 @@ function pullback_test(
             )[1]
             @test isapprox(x̄_ad, x̄_fd; rtol = rtol, atol = atol, kwargs...)
         end
+        # test that pullbacks of mutating functions undo the mutation
+        if xs[i] isa Number || xs[i] isa AbstractVector
+            @test isapprox(xs[i], xscopy[i]; rtol = rtol, atol = atol, kwargs...)
+        end
     end
+    return nothing
 end
 
 @testset "Zygote rules" begin
-    @testset for T in [Float64, ComplexF64]
-        @testset "*(::AbstractQDHT, ::Array{$T})" begin
-            rng = MersenneTwister(86)
-            @testset "Vector" begin
-                N = 64
-                q = Hankel.QDHT{1,2}(10, N)
-                fr, f̄r, f̄k = randn(rng, T, N), randn(rng, T, N), randn(rng, T, N)
-                pullback_test(*, f̄k, (q, nothing), (fr, f̄r))
-            end
+    @testset "$f(::QDHT, ::Array{$T})" for f in (*, \), T in (Float64, ComplexF64)
+        rng = MersenneTwister(86)
+        N, M, K = 64, 5, 10
+        @testset "Vector" begin
+            q = Hankel.QDHT{1,2}(10, N)
+            fr, f̄r, f̄k = ntuple(_ -> randn(rng, T, N), 3)
+            pullback_test(f, f̄k, (q, nothing), (fr, f̄r))
+        end
 
-            @testset "Matrix" begin
-                N = 64
-                M = 5
-                @testset for dim in (1, 2)
-                    q = Hankel.QDHT{1,2}(10, N; dim = dim)
-                    s = dim == 1 ? (N, M) : (M, N)
-                    fr, f̄r, f̄k = randn(rng, T, s), randn(rng, T, s), randn(rng, T, s)
-                    pullback_test(*, f̄k, (q, nothing), (fr, f̄r))
-                end
-            end
-
-            @testset "Array{$T,3}" begin
-                N = 64
-                M = 5
-                K = 10
-                q = Hankel.QDHT{1,2}(10, N; dim = 2)
-                s = (M, N, K)
-                fr, f̄r, f̄k = randn(rng, T, s), randn(rng, T, s), randn(rng, T, s)
-                pullback_test(*, f̄k, (q, nothing), (fr, f̄r))
+        @testset "Matrix" begin
+            @testset for dim in (1, 2)
+                q = Hankel.QDHT{1,2}(10, N; dim = dim)
+                s = dim == 1 ? (N, M) : (M, N)
+                fr, f̄r, f̄k = ntuple(_ -> randn(rng, T, s), 3)
+                pullback_test(f, f̄k, (q, nothing), (fr, f̄r))
             end
         end
 
-        @testset "\\(::AbstractQDHT, ::Array{$T})" begin
-            rng = MersenneTwister(62)
-            @testset "Vector" begin
-                N = 64
-                q = Hankel.QDHT{1,2}(10, N)
-                fr, f̄r, f̄k = randn(rng, T, N), randn(rng, T, N), randn(rng, T, N)
-                pullback_test(\, f̄k, (q, nothing), (fr, f̄r))
-            end
+        @testset "Array{$T,3}" begin
+            q = Hankel.QDHT{1,2}(10, N; dim = 2)
+            s = (M, N, K)
+            fr, f̄r, f̄k = ntuple(_ -> randn(rng, T, s), 3)
+            pullback_test(f, f̄k, (q, nothing), (fr, f̄r))
+        end
+    end
 
-            @testset "Matrix" begin
-                N = 64
-                M = 5
-                @testset for dim in (1, 2)
-                    q = Hankel.QDHT{1,2}(10, N; dim = dim)
-                    s = dim == 1 ? (N, M) : (M, N)
-                    fr, f̄r, f̄k = randn(rng, T, s), randn(rng, T, s), randn(rng, T, s)
-                    pullback_test(\, f̄k, (q, nothing), (fr, f̄r))
-                end
-            end
+    @testset "$f(::Array{$T}, ::QDHT, ::Array{$T})" for f in (mul!, ldiv!), T in (Float64, ComplexF64)
+        rng = MersenneTwister(86)
+        N, M, K = 64, 5, 10
+        @testset "Vector" begin
+            q = Hankel.QDHT{1,2}(10, N)
+            fr, fk, f̄r, f̄k = ntuple(_ -> randn(rng, T, N), 4)
+            pullback_test(f, f̄k, (fk, nothing), (q, nothing), (fr, f̄r))
+        end
 
-            @testset "Array{$T,3}" begin
-                N = 64
-                M = 5
-                K = 10
-                q = Hankel.QDHT{1,2}(10, N; dim = 2)
-                s = (M, N, K)
-                fr, f̄r, f̄k = randn(rng, T, s), randn(rng, T, s), randn(rng, T, s)
-                pullback_test(\, f̄k, (q, nothing), (fr, f̄r))
+        @testset "Matrix" begin
+            @testset for dim in (1, 2)
+                q = Hankel.QDHT{1,2}(10, N; dim = dim)
+                s = dim == 1 ? (N, M) : (M, N)
+                fr, fk, f̄r, f̄k = ntuple(_ -> randn(rng, T, s), 4)
+                pullback_test(f, f̄k, (fk, nothing), (q, nothing), (fr, f̄r))
             end
         end
 
-        @testset "integrateR/K(::AbstractMatrix, ::Array{$T})" begin
-            rng = MersenneTwister(27)
-            @testset "Vector" begin
-                N = 64
-                q = Hankel.QDHT{1,2}(10, N)
-                A, Ā, ȳ = randn(rng, T, N), randn(rng, T, N), randn(rng, T)
-                pullback_test(integrateR, ȳ, (A, Ā), (q, nothing))
-                pullback_test(integrateK, ȳ, (A, Ā), (q, nothing))
-            end
+        @testset "Array{$T,3}" begin
+            q = Hankel.QDHT{1,2}(10, N; dim = 2)
+            s = (M, N, K)
+            fr, fk, f̄r, f̄k = ntuple(_ -> randn(rng, T, s), 4)
+            pullback_test(f, f̄k, (fk, nothing), (q, nothing), (fr, f̄r))
+        end
+    end
 
-            @testset "Matrix" begin
-                N = 64
-                M = 5
-                @testset for dim in (1, 2)
-                    q = Hankel.QDHT{1,2}(10, N)
-                    s = dim == 1 ? (N, M) : (M, N)
-                    sy = dim == 1 ? (1, M) : (M, 1)
-                    A, Ā, ȳ = randn(rng, T, s), randn(rng, T, s), randn(rng, T, sy)
-                    pullback_test(integrateR, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = dim))
-                    pullback_test(integrateK, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = dim))
-                end
-            end
+    @testset "$f(::Array{$T}, ::QDHT)" for f in (integrateR, integrateK), T in (Float64, ComplexF64)
+        rng = MersenneTwister(27)
+        N, M, K = 64, 5, 10
+        @testset "Vector" begin
+            q = Hankel.QDHT{1,2}(10, N)
+            A, Ā, ȳ = randn(rng, T, N), randn(rng, T, N), randn(rng, T)
+            pullback_test(f, ȳ, (A, Ā), (q, nothing))
+        end
 
-            @testset "Array{$T,3}" begin
-                N = 64
-                M = 5
-                K = 10
-                q = Hankel.QDHT{1,2}(10, N)
-                s = (M, N, K)
-                sy = (M, 1, K)
+        @testset "Matrix" begin
+            @testset for dim in (1, 2)
+                q = Hankel.QDHT{1,2}(10, N; dim = dim)
+                s = dim == 1 ? (N, M) : (M, N)
+                sy = dim == 1 ? (1, M) : (M, 1)
                 A, Ā, ȳ = randn(rng, T, s), randn(rng, T, s), randn(rng, T, sy)
-                pullback_test(integrateR, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = 2))
-                pullback_test(integrateK, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = 2))
+                pullback_test(f, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = dim))
             end
+        end
+
+        @testset "Array{$T,3}" begin
+            q = Hankel.QDHT{1,2}(10, N; dim = 2)
+            s = (M, N, K)
+            sy = (M, 1, K)
+            A, Ā, ȳ = randn(rng, T, s), randn(rng, T, s), randn(rng, T, sy)
+            pullback_test(f, ȳ, (A, Ā), (q, nothing); fkwargs = (; dim = 2))
         end
     end
 end
