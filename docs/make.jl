2dddfae869763c8527f3f47328e87f1ec69490ae
Seth Axen <seth.axen@gmail.com>
2020-05-06 18:29:54 -0700

    Document the autodiff rules

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,8 @@ makedocs(
     sitename="Hankel.jl",
     pages = [
         "Documentation" => "index.md",
-        "Derivations" => "derivations.md"
+        "Derivations" => "derivations.md",
+        "Automatic Differentiation" => "autodiff.md"
     ]
 )
 
